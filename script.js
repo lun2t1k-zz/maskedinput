a14b4a4c3db588bbe90c9a6f4280743d0b6ec954
Dmitry Krutkin <krutkin@a2design.biz>
2021-09-22 13:02:33 +0600

    Added a disabled-btn style

--- script.js
+++ script.js
@@ -19,9 +19,11 @@ $(document).ready(function() {
     if (phone.length < 1) {
       $('#phone').after('<span class="error">This field is required</span>');
       $(':input[type="submit"]').prop('disabled', true);
+      $(':input[type="submit"]').addClass('disabled-btn');
       $('input[type="tel"]').keyup(function() {
         if($(this).val() != '') {
           $(':input[type="submit"]').prop('disabled', false);
+          $(':input[type="submit"]').removeClass('disabled-btn');
         }
       });
     }
@@ -29,24 +31,27 @@ $(document).ready(function() {
     if (email.length < 1) {
       $('#email').after('<span class="error">This field is required</span>');
       $(':input[type="submit"]').prop('disabled', true);
+      $(':input[type="submit"]').addClass('disabled-btn');
       $('input[type="email"]').keyup(function() {
         if($(this).val() != '') {
           $(':input[type="submit"]').prop('disabled', false);
+          $(':input[type="submit"]').removeClass('disabled-btn');
         }
       });
-    } else {
-      var regEx = /^[A-Z0-9][A-Z0-9._%+-]{0,63}@(?:[A-Z0-9-]{1,63}.){1,125}[A-Z]{2,63}$/;
-      var validEmail = regEx.test(email);
+    } 
+    // else {
+    //   var regEx = /^[A-Z0-9][A-Z0-9._%+-]{0,63}@(?:[A-Z0-9-]{1,63}.){1,125}[A-Z]{2,63}$/;
+    //   var validEmail = regEx.test(email);
       
-      if (!validEmail) {
-        $('#email').after('<span class="error">Enter a valid email</span>');
-        $(':input[type="submit"]').prop('disabled', true);
-        $('input[type="email"]').keyup(function() {
-          if($(this).val() != '') {
-            $(':input[type="submit"]').prop('disabled', false);
-          }
-        });
-      }
-    }
+    //   if (!validEmail) {
+    //     $('#email').after('<span class="error">Enter a valid email</span>');
+    //     $(':input[type="submit"]').prop('disabled', true);
+    //     $('input[type="email"]').keyup(function() {
+    //       if($(this).val() != '') {
+    //         $(':input[type="submit"]').prop('disabled', false);
+    //       }
+    //     });
+    //   }
+    // }
   });
 });
