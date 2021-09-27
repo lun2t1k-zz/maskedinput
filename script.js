f2c70d691ebaa5fd4684d070a70983b7444b6dce
Dmitry Krutkin <84284545+lun2t1k@users.noreply.github.com>
2021-09-27 14:16:05 +0600

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
       });
     } 
     else {
-      var regEx = /^[A-Z0-9][A-Z0-9._%+-]{0,63}@(?:[A-Z0-9-]{1,63}.){1,125}[A-Z]{2,63}$/;
+      var regEx = /[a-za-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?/;
       var validEmail = regEx.test(email);
       
       if (!validEmail) {
